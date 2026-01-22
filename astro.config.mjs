9b243cc849ab6edafcbbca8c3f1a46f617ca6d6d
Yousuf Khan <md-yousuf.khan@kuehne-nagel.com>
2026-01-22 15:49:53 +0100

    feat: Update icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,4 +8,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [icon()],
 });
